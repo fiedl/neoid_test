98be11d88ade58d8b7d177d92e96d2c10f66abe1
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-03-28 22:06:41 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeoidTest::Application.config.secret_token = 'c62df781adf1cb8e6a42497049c6c509244783fbb16ed8ee93df6087f5b53b1529a27eaf5b5d6419bf909366773606363e6d0513ea6a955114986dc57bfa82bd'
